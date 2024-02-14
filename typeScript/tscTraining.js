4673cbcdc68d52abe1a7d2207efe39b693fe2e57
kfir <kfir.kotler@gmail.com>
2024-02-14 15:01:10 +0200

    For class

---
+++ typeScript/tscTraining.js
@@ -0,0 +1,9 @@
+"use strict";
+let greeting = 12345;
+console.log(greeting);
+let firstNumber = "100";
+let secondNumber = 50;
+let result = firstNumber + secondNumber;
+console.log(typeof (result));
+let isBool = true;
+console.log(isBool);
---
+++ typeScript/tscTraining.ts
@@ -0,0 +1,11 @@
+let greeting: number = 12345
+console.log(greeting);
+
+let firstNumber: string = "100"
+let secondNumber: number = 50
+let result = firstNumber + secondNumber
+
+console.log(typeof (result));
+
+let isBool: boolean = true
+console.log(isBool);
